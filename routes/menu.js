a547244939231a1aaa0d7f8e8ab756c60b828a82
ShayaanAkbar <shayaanakbar1106@gmail.com>
2021-05-11 19:25:55 -0400

    Database connected testing get route for menus successful

---
+++ routes/menu.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mySqlConnection = require('../connection');
+const Router = express.Router();
+
+// get all menus
+Router.get('/', (req, res) => {
+    mySqlConnection.query("SELECT * from menus", (err, rows, fields) => {
+        if(!err) {
+            res.send(rows);
+        } else {
+            console.log(err)
+        }
+    })
+})
+
+module.exports = Router
--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
 
-app.listen('5000', () => {
-    console.log('server started on port 5000')
+app.listen('5001', () => {
+    console.log('server started on port 5001')
 });
